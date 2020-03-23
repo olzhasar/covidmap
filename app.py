db494a8a50ca32c3b78fbc06451c03abfdc8fbf6
Olzhas <o.arystanov@gmail.com>
2020-03-24 02:48:32 +0600

    Updated layout

--- app.py
+++ app.py
@@ -47,7 +47,14 @@ def get_current_data():
     data = (
         CaseData.query.group_by("location_id")
         .join(CaseData.location)
-        .values("confirmed", "location.name", "location.latitude", "location.longitude")
+        .values(
+            "confirmed",
+            "recovered",
+            "fatal",
+            "location.name",
+            "location.latitude",
+            "location.longitude",
+        )
     )
     df = pd.DataFrame(data)
     return df
@@ -57,22 +64,56 @@ historical = get_historical_data()
 df = get_current_data()
 dates = historical.index.strftime("%Y-%m-%d").tolist()
 
-fig = px.scatter_mapbox(
+map_fig = px.scatter_mapbox(
     df,
     lat="location.latitude",
     lon="location.longitude",
     hover_name="location.name",
+    hover_data=["confirmed", "recovered", "fatal"],
+    labels={
+        "confirmed": "Зарегистрированных",
+        "recovered": "Выздоровевших",
+        "fatal": "Смертей",
+    },
     size="confirmed",
     zoom=4,
+    mapbox_style="dark",
     center={"lat": 48.0196, "lon": 66.9237},
     opacity=0.7,
     height=700,
     color_discrete_sequence=["rgba(230, 0, 0, .7)"],
 )
 
+map_fig.data[
+    0
+].hovertemplate = "<b>%{hovertext}</b><br><br>Зарегистрированных: %{customdata[0]}<br>Выздоровевших: %{customdata[1]}<br>Смертей: %{customdata[2]}"
 
-fig.update_layout(mapbox_style="dark", mapbox_accesstoken=server.config["MAPBOX_TOKEN"])
-fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+map_fig.update_layout(
+    mapbox_accesstoken=server.config["MAPBOX_TOKEN"],
+    hoverlabel={
+        "bgcolor": "#1a1c23",
+        "bordercolor": "#bdbdbd",
+        "font": {"family": "'Roboto Slab', sans-serif", "color": "#bdbdbd", "size": 18},
+    },
+    margin={"r": 0, "t": 0, "l": 0, "b": 0},
+    font={"family": "'Roboto Slab', sans-serif", "color": "#bdbdbd"},
+)
+
+
+chart_fig = px.bar(
+    x=dates,
+    y=historical.confirmed.tolist(),
+    color_discrete_sequence=["rgba(255, 170, 0, .7)"],
+    height=700,
+)
+chart_fig.update_layout(
+    paper_bgcolor="#22252b",
+    plot_bgcolor="rgba(0,0,0,0)",
+    grid=None,
+    xaxis={"title": "Дата", "showgrid": False, "nticks": 5, "tickformat": "%d-%m"},
+    yaxis={"title": "Общее количество случаев", "showgrid": False},
+    font={"family": "'Roboto Slab', sans-serif", "color": "#bdbdbd"},
+)
 
 app = dash.Dash(
     "COVID-19 Map Kazakhstan",
@@ -135,30 +176,9 @@ app.layout = html.Div(
             ],
             className="left-col",
         ),
-        html.Div(children=[dcc.Graph(id="map", figure=fig),], className="main-col"),
+        html.Div(children=[dcc.Graph(id="map", figure=map_fig),], className="main-col"),
         html.Div(
-            children=[
-                dcc.Graph(
-                    id="dynamics-graph",
-                    figure={
-                        "data": [
-                            {
-                                "x": dates,
-                                "y": historical.confirmed.tolist(),
-                                "type": "scatter",
-                                "marker": {"color": "rgb(255, 170, 0)"},
-                                "name": "Confirmed Cases",
-                                "trendline": "ols",
-                            },
-                        ],
-                        "layout": {
-                            "height": "700",
-                            "paper_bgcolor": "#22252b",
-                            "plot_bgcolor": "rgba(0,0,0,0)",
-                        },
-                    },
-                ),
-            ],
+            children=[dcc.Graph(id="dynamics-graph", figure=chart_fig),],
             className="right-col",
         ),
         html.Div(
