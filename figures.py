6c344f19e4dca02a6c26394386b26f0a80deb726
Olzhas <o.arystanov@gmail.com>
2020-03-24 20:45:17 +0600

    Chart figure - changed hover template

--- figures.py
+++ figures.py
@@ -48,8 +48,15 @@ chart_fig = px.bar(
 chart_fig.update_layout(
     paper_bgcolor="#22252b",
     plot_bgcolor="rgba(0,0,0,0)",
+    hoverlabel={
+        "bgcolor": "#1a1c23",
+        "bordercolor": "#bdbdbd",
+        "font": {"family": "'Roboto Slab', sans-serif", "color": "#bdbdbd", "size": 18},
+    },
     grid=None,
-    xaxis={"title": "Дата", "showgrid": False, "nticks": 5, "tickformat": "%d-%m"},
+    xaxis={"title": "Дата", "showgrid": False, "nticks": 5, "tickformat": "%d-%m-%Y"},
     yaxis={"title": "Общее количество случаев", "showgrid": False},
     font={"family": "'Roboto Slab', sans-serif", "color": "#bdbdbd"},
 )
+
+chart_fig.data[0].hovertemplate = '%{x}:  <b style="color: rgb(230,0,0);">%{y}</b>'
