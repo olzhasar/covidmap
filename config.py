5b141d7f0647d8753fc851d328b159746e13cdc2
Olzhas <o.arystanov@gmail.com>
2020-03-27 23:28:10 +0600

    Added authentication

--- config.py
+++ config.py
@@ -6,13 +6,22 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY", "COVIDSECRETSUPER123")
     SESSION_TYPE = "filesystem"
+
     FLASK_ADMIN_SWATCH = "cerulean"
+
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL", "sqlite:///" + os.path.join(BASEDIR, "db.sqlite3")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     MAPBOX_TOKEN = os.getenv("MAPBOX_TOKEN")
     MAPBOX_STYLE_URL = os.getenv("MAPBOX_STYLE_URL", "dark")
+
     CACHE_TYPE = "filesystem"
-    CACHE_DEFAULT_TIMEOUT = 3
-    CACHE_DIR = os.path.join(BASEDIR, 'cache')
+    CACHE_DEFAULT_TIMEOUT = os.getenv("CACHE_TIMEOUT", 600)
+    CACHE_DIR = os.path.join(BASEDIR, "cache")
+
+    BASIC_AUTH_USERNAME = os.getenv("AUTH_USERNAME", "admin")
+    BASIC_AUTH_PASSWORD = os.getenv("AUTH_PASSWORD", "covidmap")
+
+    ADMIN_URL = os.getenv("ADMIN_URL", "admin")
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, Response, redirect
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib.sqla import ModelView as BaseModelView
+from flask_basicauth import BasicAuth
 from flask_caching import Cache
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import HTTPException
 
 from config import Config
 
@@ -12,13 +14,44 @@ server.config.from_object(Config)
 
 cache = Cache(server)
 
-admin = Admin(server, name="covidmap", template_mode="bootstrap3", url="/manage1618")
+auth = BasicAuth(server)
 
 db = SQLAlchemy(server)
 migrate = Migrate(server, db)
 
-from models import CaseData, Location  # noqa E402
+from models import CaseData, Location  # noqa E402 isort:skip
 
 
+class AuthException(HTTPException):
+    def __init__(self, message):
+        super().__init__(
+            message,
+            response=Response(
+                message, 401, {"WWW-Authenticate": 'Basic realm="Login Required"'}
+            ),
+        )
+
+
+@server.route("/logout")
+def logout():
+    raise AuthException("You have been logged out")
+
+
+class ModelView(BaseModelView):
+    def is_accessible(self):
+        if not auth.authenticate():
+            raise AuthException("Access denied")
+        return True
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(auth.challenge())
+
+
+admin = Admin(
+    server,
+    name="covidmap",
+    template_mode="bootstrap3",
+    url=f"/{server.config['ADMIN_URL']}",
+)
 admin.add_view(ModelView(Location, db.session))
 admin.add_view(ModelView(CaseData, db.session))
