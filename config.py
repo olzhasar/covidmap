98c46584cf95a655cbd59b559be77a69e2b23601
Olzhas <o.arystanov@gmail.com>
2020-03-29 03:04:29 +0600

    Cache - filesystem

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     MAPBOX_TOKEN = os.getenv("MAPBOX_TOKEN")
     MAPBOX_STYLE_URL = os.getenv("MAPBOX_STYLE_URL", "dark")
 
-    CACHE_TYPE = os.getenv("CACHE_TYPE", "redis")
+    CACHE_TYPE = os.getenv("CACHE_TYPE", "filesystem")
     CACHE_DEFAULT_TIMEOUT = int(os.getenv("CACHE_TIMEOUT", 600))
     CACHE_DIR = os.path.join(BASEDIR, "cache")
 
