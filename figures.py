3cc012a8fd70832306f41a0fdda65337b3cc2e87
Olzhas <o.arystanov@gmail.com>
2020-04-02 10:46:48 +0600

    Map figure sizeref

--- figures.py
+++ figures.py
@@ -34,6 +34,7 @@ def get_map(end_date=None):
                 opacity=0.4,
                 size=current_data["confirmed"],
                 sizemin=10,
+                sizeref=2 * current_data["confirmed"].max() / (12 ** 2),
             ),
         )
     )
