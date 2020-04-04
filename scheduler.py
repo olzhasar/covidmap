3c94cfa3c0b7bee1e21feb01276b2fb8fbf75bb2
Olzhas <o.arystanov@gmail.com>
2020-04-04 22:40:47 +0600

    Scheduler - clear cache every midnight

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,19 @@
 import atexit
 
+import pytz
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from server import server
+from server import cache, server
 from updater import update_data
 
 if __name__ == "__main__":
 
-    scheduler = BlockingScheduler()
+    scheduler = BlockingScheduler(timezone=pytz.timezone("Asia/Almaty"))
     atexit.register(lambda: scheduler.shutdown())
 
     scheduler.add_job(
         func=update_data, trigger="interval", minutes=server.config["FETCH_INTERVAL"]
     )
+    scheduler.add_job(func=cache.clear, trigger="cron", minute=0, hour=0)
+
     scheduler.start()
