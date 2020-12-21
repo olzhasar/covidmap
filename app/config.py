1bd87d8d8730f9cae22db88b85794308203d57a1
olzhasar <o.arystanov@gmail.com>
2020-12-22 01:32:20 +0300

    Moved load_dotenv to config class

--- app/config.py
+++ app/config.py
@@ -5,10 +5,9 @@ from dotenv import load_dotenv
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
-load_dotenv()
-
-
 class Config(object):
+    load_dotenv()
+
     SECRET_KEY = os.getenv("SECRET_KEY", "COVIDSECRETSUPER123")
     SESSION_TYPE = "filesystem"
 
