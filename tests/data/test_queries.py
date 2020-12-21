b5f7ac534243334187187825f62c87fe995c5ea6
olzhasar <o.arystanov@gmail.com>
2020-12-21 23:05:09 +0300

    Updated queries tests

--- tests/data/test_queries.py
+++ tests/data/test_queries.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 import pandas as pd
-import pytest
 import pytz
 
 from data import queries
@@ -52,49 +51,30 @@ class TestGetLocationsDF:
             i += 1
 
 
-@pytest.mark.skip("fuck")
 class TestGetDF:
-    def test_empty(self, use_db):
-        df = queries.get_df()
-
-        assert isinstance(df, pd.DataFrame)
-        assert df.empty
-
     def test_with_data(self, use_db):
         location = LocationFactory(id=1)
 
-        records = CaseDataFactory.create_batch(5, location_id=1)
+        records = CaseDataFactory.create_batch(5, location=location)
         records = sorted(records, key=lambda x: x.date)
 
         df = queries.get_df()
 
         assert isinstance(df, pd.DataFrame)
         assert not df.empty
+
+        assert list(df.index.names) == ["location_id", "date"]
         assert list(df.columns) == [
-            "date",
             "confirmed",
             "recovered",
             "fatal",
-            "location.id",
-            "location.name",
-            "location.latitude",
-            "location.longitude",
+            "confirmed_cumulative",
+            "recovered_cumulative",
+            "fatal_cumulative",
+            "name",
+            "latitude",
+            "longitude",
         ]
-
-        i = 0
-        for _, row in df.iterrows():
-            record = records[i]
-
-            assert row["date"] == record.date
-            assert row["confirmed"] == record.confirmed
-            assert row["recovered"] == record.recovered
-            assert row["fatal"] == record.fatal
-            assert row["location.id"] == location.id
-            assert row["location.name"] == location.name
-            assert row["location.latitude"] == float(location.latitude)
-            assert row["location.longitude"] == float(location.longitude)
-
-            i += 1
 
 
 class TestGetUpdatedAt:
