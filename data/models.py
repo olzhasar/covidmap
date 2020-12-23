119a216723ee6a0c43a5c32011573e5da4efa2ca
olzhasar <o.arystanov@gmail.com>
2020-12-23 13:08:32 +0300

    Models - moved relationship from Location to CaseData

--- data/models.py
+++ data/models.py
@@ -8,7 +8,6 @@ class Location(db.Model):
     name = db.Column(db.String(255), index=True, unique=True)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
-    case_data = db.relationship("CaseData", backref="location", lazy="dynamic")
 
     api_id = db.Column(db.Integer, index=True, unique=True)
     api_name = db.Column(db.String(255), index=True, unique=True)
@@ -23,6 +22,7 @@ class CaseData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, index=True, default=date.today, nullable=False)
     location_id = db.Column(db.Integer, db.ForeignKey("location.id"), nullable=False)
+    location = db.relationship("Location", lazy=True)
     confirmed = db.Column(db.Integer, default=0)
     recovered = db.Column(db.Integer, default=0)
     fatal = db.Column(db.Integer, default=0)
